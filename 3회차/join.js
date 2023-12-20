4d6884d46c00b74b9251ba25ca188258eb9a50bd
minyoung <alsendrha@naver.com>
2023-12-20 13:33:53 +0900

    3회차까지 완료

---
+++ 3회차/join.js
@@ -0,0 +1,54 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // 새로고침 차단
+
+    let userId = event.target.id.value;
+    let userPw1 = event.target.pw1.value;
+    let userPw2 = event.target.pw2.value;
+    let userName = event.target.name.value;
+    let userPhone = event.target.phone.value;
+    let userPosition = event.target.position.value;
+    let userGender = event.target.gender.value;
+    let userEmail = event.target.email.value;
+    let userIntro = event.target.intro.value;
+
+    if (userId.length < 6) {
+        alert('아이디가 너무 짧습니다. 6자 이상 입력해주세요.');
+        return;
+    }
+
+    if (userPw1 !== userPw2) {
+        alert('비밀번호가 일치하지 않습니다.');
+        return;
+    }
+
+    if (userName.length < 2) {
+        alert('이름이 너무 짧습니다. 2자 이상 입력해주세요.');
+        return;
+    }
+
+    if (userPhone.length < 10) {
+        alert('전화번호가 너무 짧습니다. 10자 이상 입력해주세요.');
+        return;
+    }
+
+    if (userEmail.length < 10) {
+        alert('이메일이 너무 짧습니다. 10자 이상 입력해주세요.');
+        return;
+    }
+
+    if (userIntro.length < 10) {
+        alert('자기소개가 너무 짧습니다. 10자 이상 입력해주세요.');
+        return;
+    }
+
+    //가입 환영 인사
+    document.body.innerHTML = '';
+    document.write(`<p>${userId}님 환영합니다!</p>`)
+    document.write('<p>회원 가입 시 입력하신 내역은 다음과 같습니다.</p>')
+    document.write(`<p>아이디 : ${userId}</p>`)
+    document.write(`<p>이름 : ${userName}</p>`)
+    document.write(`<p>전화번호 : ${userPhone}</p>`)
+    document.write(`<p>원하는 직무 : ${userPosition}</p>`)
+});
